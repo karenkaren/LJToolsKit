6b3e09f30db0509595a388d959e74fa2fd80a34b
liujie <karenzaixian.163.com>
2018-03-09 14:16:41 +0800

    first commit

---
+++ Example/Pods/Target Support Files/LJToolsKit/LJToolsKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LJToolsKit : NSObject
+@end
+@implementation PodsDummy_LJToolsKit
+@end
---
+++ Example/Pods/Target Support Files/Pods-LJToolsKit_Tests/Pods-LJToolsKit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LJToolsKit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_LJToolsKit_Tests
+@end
